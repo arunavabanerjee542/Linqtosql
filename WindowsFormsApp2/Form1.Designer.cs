6ee849150cfc5bae1bb19ea5d9026f2c9f4c6fc8
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2020-12-01 23:29:45 -0800

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.d = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.idbox = new System.Windows.Forms.TextBox();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.roombox = new System.Windows.Forms.TextBox();
+            this.citybox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // d
+            // 
+            this.d.AutoSize = true;
+            this.d.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d.Location = new System.Drawing.Point(232, 21);
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(339, 31);
+            this.d.TabIndex = 0;
+            this.d.Text = "RESTAURANT DETAILS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(306, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // idbox
+            // 
+            this.idbox.Location = new System.Drawing.Point(470, 119);
+            this.idbox.Name = "idbox";
+            this.idbox.Size = new System.Drawing.Size(100, 20);
+            this.idbox.TabIndex = 2;
+            // 
+            // namebox
+            // 
+            this.namebox.Location = new System.Drawing.Point(470, 165);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(100, 20);
+            this.namebox.TabIndex = 2;
+            // 
+            // roombox
+            // 
+            this.roombox.Location = new System.Drawing.Point(470, 211);
+            this.roombox.Name = "roombox";
+            this.roombox.Size = new System.Drawing.Size(100, 20);
+            this.roombox.TabIndex = 2;
+            // 
+            // citybox
+            // 
+            this.citybox.Location = new System.Drawing.Point(470, 263);
+            this.citybox.Name = "citybox";
+            this.citybox.Size = new System.Drawing.Size(100, 20);
+            this.citybox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NAME";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ROOMS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(313, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "CITY";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "PREVIOUS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "NEXT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.citybox);
+            this.Controls.Add(this.roombox);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.idbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.d);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label d;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idbox;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.TextBox roombox;
+        private System.Windows.Forms.TextBox citybox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        RestaurantDBDataContext dc;
+
+        List<Restaurant> l;
+
+        int index = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* RestaurantDBDataContext dc = new RestaurantDBDataContext();
+             dataGridView1.DataSource = dc.Restaurants; */
+
+            dc = new RestaurantDBDataContext();
+
+            l = dc.Restaurants.ToList();
+
+            disp();
+
+        }
+
+
+        public void disp()
+        {
+            idbox.Text = l[index].Id.ToString();
+            roombox.Text = l[index].rooms.ToString();
+            namebox.Text = l[index].name.ToString();
+            citybox.Text = l[index].city.ToString();
+
+
+
+        }
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(index< l.Count()-1)
+            {
+                index++;
+                disp();
+            }
+
+            else
+            {
+                MessageBox.Show(" No More Records ");
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (index > 0)
+            {
+                index--;
+                disp();
+            }
+
+            else
+            {
+                MessageBox.Show(" No Previous Records ");
+            }
+
+
+
+
+        }
+    }
+}
